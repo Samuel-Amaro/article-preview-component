d84f250e1415611eab83bba31d0fe7996d921fa1
Samuel Amaro <samuelamaro96746313@gmail.com>
2022-07-06 09:40:38 -0300

    project finished

--- js/script.js
+++ js/script.js
@@ -1,18 +1,16 @@
+"use strict";
+
 let btn = document.querySelector(".button-share");
 let iconBtnShare = btn.querySelector("img");
 let dataProfile = document.querySelector(".profile__data");
 let optionShare = document.querySelector(".option-share-hidden");
 let profile = document.querySelector(".profile");
-let coordsBtnShare = btn.getBoundingClientRect();
 
-function positionTooltipShare(positionX, positionY) {
-    console.log("positionX: " + positionX + " positionY: " + positionY);
-    let y = positionY - optionShare.offsetHeight - 25;
+function positionTooltipShare() {
+    let coordsBtnShare = btn.getBoundingClientRect();
+    let y = coordsBtnShare.y - optionShare.offsetHeight - btn.offsetHeight + 10;
     let x = optionShare.offsetWidth / 2;
-    console.log("x: " + x + "/y: " + y);
-    console.log(btn.clientWidth / 2);
-    //TODO: centralizar o tooltip em relação ao button
-    optionShare.style.left = positionX - x  + "px";
+    optionShare.style.left = coordsBtnShare.x - x + (coordsBtnShare.width / 2) + "px";
     optionShare.style.top = y + "px";
 }
 
@@ -41,8 +39,7 @@ btn.addEventListener("click", (event) => {
         if(optionShare.classList.contains("option-share-hidden")) {
             optionShare.classList.remove("option-share-hidden");
             optionShare.classList.add("option-share-view");
-            //TODO: encontrar uma forma correta de como posiconar o tooltip em relação ao btn, a cada resize da janela permanecer no mesmo lugar
-            positionTooltipShare(elemAninhadoBtn, event.pageY);
+            positionTooltipShare();
             btn.classList.add("button-share_click-modifier");
             iconBtnShare.src = "./images/icon-share-white.svg";
         }else{
